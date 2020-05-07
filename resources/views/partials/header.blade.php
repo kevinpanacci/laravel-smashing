e83a1fd4fb8c96496fa92c5d1b91b82f21004453
Kevin <kevinpanacci@hotmail.it>
2020-05-07 11:04:01 +0200

    header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,36 @@
+<div class="header">
+    <div class="logo">
+        <img src="https://d33wubrfki0l68.cloudfront.net/3fbf6ef77e5e00aea7a22423087373169b52ae7e/413ff/images/logo/logo.svg" alt="">
+    </div>
+    <nav>
+        <ul>
+            <li>
+                <a href="#">Articles</a>
+            </li>
+            <li>
+                <a href="#">Guides</a>
+            </li>
+            <li>
+                <a href="#">Books</a>
+            </li>
+            <li>
+                <a href="#">Events</a>
+            </li>
+            <li>
+                <a href="#">Membership</a>
+            </li>
+            <li>
+                <a href="#">Jobs</a>
+            </li>
+            <li>
+                <a href="#">Newsletter</a>
+            </li>
+            <li>
+                <a href="#">Podcasts</a>
+            </li>
+            <li>
+                <a href="#">More</a>
+            </li>
+        </ul>
+    </nav>
+</div>
